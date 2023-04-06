6dd22d888a0bbbdfabade40b9a35d952ef52663b
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-04-06 16:07:47 -0600

    refactor(users): reformat code

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
-const config = require('./config');
-const db = require('./db');
-const jwt = require('./jwt');
-const HTTPError = require('./httpError');
+const config = require('./config')
+const db = require('./db')
+const jwt = require('./jwt')
+const HTTPError = require('./httpError')
 
 module.exports = { config, db, HTTPError, jwt }
--- src/usecases/user/index.js
+++ src/usecases/user/index.js
@@ -1,46 +1,42 @@
-const bcrypt = require('bcrypt');
-const { HTTPError, jwt } = require('../../lib');
-const { User } = require('../../models');
+const bcrypt = require('bcrypt')
+const { HTTPError, jwt } = require('../../lib')
+const { User } = require('../../models')
 
 const login = async (email, password) => {
-    const userDb = await User.findOne({ email }).lean();
-    if (!userDb) throw new HTTPError(404, 'User not found, please create account');
-    if (!bcrypt.compareSync(password, userDb.password)) throw new HTTPError(401, 'Email or password is incorrect');
+    const userDb = await User.findOne({ email }).lean()
+    if (!userDb)
+        throw new HTTPError(404, 'User not found, please create account')
+    if (!bcrypt.compareSync(password, userDb.password))
+        throw new HTTPError(401, 'Email or password is incorrect')
     const user = {
         firstName: userDb.firstName,
         lastName: userDb.lastName,
-        role: userDb.role
-    };
+        role: userDb.role,
+    }
     const token = jwt.createToken({
         userId: userDb._id,
-        role: userDb.role
-    });
+        role: userDb.role,
+    })
 
-    return {
-        user,
-        token
-    }
+    return { user, token }
 }
 
 const renewTokenInfo = async (userId) => {
-    const userDb = await User.findById(userId).lean();
-    if (!userDb) throw new HTTPError(404, 'User not found');
+    const userDb = await User.findById(userId).lean()
+    if (!userDb) throw new HTTPError(404, 'User not found')
     const user = {
         firstName: userDb.firstName,
         lastName: userDb.lastName,
-        role: userDb.role
-    };
+        role: userDb.role,
+    }
     const token = jwt.createToken({
         userId: userDb._id,
-        role: userDb.role
-    });
-    return {
-        user,
-        token
-    }
+        role: userDb.role,
+    })
+    return { user, token }
 }
 
 module.exports = {
     login,
-    renewTokenInfo
+    renewTokenInfo,
 }
