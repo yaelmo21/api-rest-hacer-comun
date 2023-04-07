3e3a9d41e82e80f66b22e33aef7b80e96e6a4e06
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-04-07 11:04:59 -0600

    chore(auth): reformat auth middleware code

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,36 +1,38 @@
-const { jwt } = require('../lib');
-const { User } = require('../models');
-const { verifyToken } = jwt;
+const { verifyToken } = require('../lib').jwt
+const { User } = require('../models')
 
 const authClientHandler = async (req, res, next) => {
-    const { authorization = '' } = req.headers;
-    const token = authorization.split(" ")[1];
+    const { authorization = '' } = req.headers
+    const token = authorization.split(' ')[1]
     try {
-        req.params.token = verifyToken(token);
-        next();
+        req.params.token = verifyToken(token)
+        next()
     } catch (error) {
-        const { message } = error;
-        res.status(401).json({ ok: false, message });
+        const { message } = error
+        res.status(401).json({ ok: false, message })
     }
-};
+}
 
 const authAdminHandler = async (req, res, next) => {
-    const { authorization } = req.headers;
-    const token = authorization.split(" ")[1];
+    const { authorization } = req.headers
+    const token = authorization.split(' ')[1]
     try {
-        const userDataToken = verifyToken(token);
-        const userDb = await User.findById(userDataToken.userId);
-        if (!userDb) return res.status(401).json({ ok: false, message: 'unauthorized user' });
+        const userDataToken = verifyToken(token)
+        const userDb = await User.findById(userDataToken.userId)
+        if (!userDb)
+            return res
+                .status(401)
+                .json({ ok: false, message: 'unauthorized user' })
         if (userDataToken.role === 'admin' && userDb.role === 'admin') {
-            req.params.token = userDataToken;
-            next();
-            return;
+            req.params.token = userDataToken
+            next()
+            return
         }
-        return res.status(401).json({ ok: false, message: 'unauthorized user' });
+        return res.status(401).json({ ok: false, message: 'unauthorized user' })
     } catch (error) {
-        const { message } = error;
-        return res.status(401).json({ ok: false, message });
+        const { message } = error
+        return res.status(401).json({ ok: false, message })
     }
-};
+}
 
-module.exports = { authClientHandler, authAdminHandler };
+module.exports = { authClientHandler, authAdminHandler }
