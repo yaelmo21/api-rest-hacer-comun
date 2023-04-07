ddefd2f9e93e10ed4be07aec48431ad001c644ac
Yael Ruiz <yaelruiz99@outlook.com>
2023-04-07 12:19:46 -0600

    Updated Readme.md and Model ShippingAddress

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 const User = require('./user');
 const Product = require('./product');
+const ShippingAddress = require('./shippingAddress');
 
 module.exports = {
     User,
     Product,
+    ShippingAddress
 }
