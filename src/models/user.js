56d342a4b2d3b6322a4fece9a9a145ae9375217f
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-04-06 20:06:26 -0600

    chore(users): reformat user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,33 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
 
-const userSchema = new Schema({
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, trim: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        enum: {
-            values: ['admin', 'client', 'super-user', 'SEO'],
-            message: '{VALUE} no es un rol valido',
-            default: 'client',
+const userSchema = new Schema(
+    {
+        firstName: { type: String, required: true, trim: true },
+        lastName: { type: String, trim: true },
+        email: {
+            type: String,
             required: true,
-        }
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: {
+                values: ['admin', 'client', 'super-user', 'SEO'],
+                message: '{VALUE} no es un rol valido',
+                default: 'client',
+                required: true,
+            },
+        },
+    },
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true,
-});
+)
 
-const User = mongoose.models.User || model('User', userSchema);
-module.exports = User;
+const User = mongoose.models.User || model('User', userSchema)
+module.exports = User
