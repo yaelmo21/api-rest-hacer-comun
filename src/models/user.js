9c6ed7aba357bc4d637fa2f0a40f85c5a9c4a979
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-04-08 15:26:03 -0600

    chore(users): reformat seed & model

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { Schema, model } = mongoose
-const mongoosePaginate = require('mongoose-paginate-v2');
-const { config } = require('../lib');
+const mongoosePaginate = require('mongoose-paginate-v2')
+const { config } = require('../lib')
 
 const Roles = {
     admin: 'admin',
@@ -36,18 +36,16 @@ const userSchema = new Schema(
     {
         timestamps: true,
     }
-);
-userSchema.index({ firstName: 'text', lastName: 'text', email: 'text' });
-userSchema.plugin(mongoosePaginate);
-
+)
+userSchema.index({ firstName: 'text', lastName: 'text', email: 'text' })
+userSchema.plugin(mongoosePaginate)
 
 userSchema.methods.toJSON = function () {
-    let user = this;
-    let userObj = user.toObject();
-    userObj.url = `${config.app.host}/users/${user._id}`;
-    return userObj;
+    let user = this
+    let userObj = user.toObject()
+    userObj.url = `${config.app.host}/users/${user._id}`
+    return userObj
 }
-
 
 const User = mongoose.models.User || model('User', userSchema)
 module.exports = { User, Roles }
