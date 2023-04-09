67c472c9651c1534f2259edf8fe6a87249844376
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-04-08 21:36:51 -0600

    feat(lib): add mail library

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,6 @@ const db = require('./db')
 const jwt = require('./jwt')
 const HTTPError = require('./httpError')
 const cryptography = require('./cryptography')
+const mail = require('./mail')
 
-module.exports = { config, db, HTTPError, jwt, cryptography }
+module.exports = { config, db, HTTPError, jwt, cryptography, mail }
