b2712a4185b3f3ba66ce4035d48e737974b1db1b
Yael Ruiz <yaelruiz99@outlook.com>
2023-04-28 19:43:42 -0600

    feat: Webhook Complete pay Cart

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middlewares');
-const { HTTPError, config } = require('../lib');
-const payCases = require('../usecases/pay');
+const { HTTPError, } = require('../lib');
+const paymentsCases = require('../usecases/payments');
 
-router.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
-    const sig = request.headers['stripe-signature'];
-    let event;
+
+
+
+router.post('/webhook', express.raw({ type: 'application/json' }), async (request, response) => {
     try {
-        event = stripe.webhooks.constructEvent(request.body, sig, config.stripe.privateApiKey);
-    } catch (err) {
-        response.status(400).send(`Webhook Error: ${err.message}`);
-        return;
+        const signature = request.headers['stripe-signature'];
+        const body = request.rawBody;
+        await paymentsCases.captureActions(body, signature);
+        response.send();
+    } catch (error) {
+        if (HTTPError.isHttpError(error)) {
+            return response.status(error.statusCode).json({ message: error.message });
+        }
+        return response.status(500).json({
+            message: 'Internal Server Error, contact Support'
+        });
     }
-    // Handle the event
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntentSucceeded = event.data.object;
-            // Then define and call a function to handle the event payment_intent.succeeded
-            break;
-        // ... handle other event types
-        default:
-
-    }
-    response.send();
 });
 
 router.post('/portal-session', auth.authHandler, async (req, res) => {
--- src/usecases/payments/index.js
+++ src/usecases/payments/index.js
@@ -1,4 +1,5 @@
 const { pay, HTTPError } = require('../../lib');
+const orderCases = require('../order');
 
 
 const getEvent = (body, signature) => {
@@ -10,6 +11,19 @@ const getEvent = (body, signature) => {
     }
 }
 
-const captureActions = (body, signature) => {
+const captureActions = async (body, signature) => {
     const event = getEvent(body, signature);
+    switch (event.type) {
+        case 'checkout.session.completed':
+            const sessionId = event.data.object.id;
+            const session = await pay.retriveSessionCustomer(sessionId);
+            const result = await orderCases.updatePayOrder(session.id);
+            return result;
+        default:
+            throw new HTTPError(400, `Unhandled event type ${event.type}`);
+    }
+}
+
+module.exports = {
+    captureActions
 }
